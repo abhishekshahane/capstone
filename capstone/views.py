45107492fd77a44785f7f6a7e517bb1be246650f
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2022-08-03 23:28:31 -0400

    added list of cities

--- capstone/views.py
+++ capstone/views.py
@@ -1,5 +1,9 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import list_of_cities
 # Create your views here.
+cities = list_of_cities.thousands_of_cities
 def index(request):
-    return HttpResponse("hey!")
+    print(cities)
+    return HttpResponse('good')
