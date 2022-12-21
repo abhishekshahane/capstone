6e915bd9a3d69c520f247055e5dcb43f43936591
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2022-12-21 15:04:02 -0500

    added slight template

--- capstone/views.py
+++ capstone/views.py
@@ -5,4 +5,6 @@ from . import list_of_cities
 # Create your views here.
 cities = list_of_cities.thousands_of_cities
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'cities': cities
+    })
