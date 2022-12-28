1f5b83ca25436e4047369472398752707777e227
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2022-12-28 16:56:36 -0500

    model user

--- capstone/models.py
+++ capstone/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class WUser(models.Model):
+    closest_destination = models.CharField(max_length=40)
+    user_destination = models.CharField(max_length=40)
+
 # Create your models here.
--- capstone/views.py
+++ capstone/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import list_of_cities
+from .models import WUser
 # Create your views here.
 cities = list_of_cities.thousands_of_cities
 def index(request):
